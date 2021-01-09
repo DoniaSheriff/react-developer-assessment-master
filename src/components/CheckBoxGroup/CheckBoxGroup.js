61370fd13e7632a2c22a411fb6da274ef5105957
Donia Sherif <65144685+DoniaSherif@users.noreply.github.com>
2021-01-09 17:59:46 +0200

    clean code

--- src/components/CheckBoxGroup/CheckBoxGroup.js
+++ src/components/CheckBoxGroup/CheckBoxGroup.js
@@ -1,19 +1,23 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import CheckBox from "./../CheckBox/CheckBox";
 
-export default ({ options, ...props }) => (
-  <div
-    className="ant-checkbox-group"
-    style={{   marginRight: 10 }}
-  >
-    {options.map(label => (
-      <CheckBox
-        key={label}
-        label={label}
-        disabled={props.disabled}
-        handleChange={props.handleChange}
-        value={props[label]}
-      />
-    ))}
-  </div>
-);
+export default ({ options, ...props }) => {
+  return (
+    <div
+      className="ant-checkbox-group"
+      style={{
+        marginRight: 10}}
+
+    >
+      {options.map(label => (
+        <CheckBox
+          key={label}
+          label={label}
+          disabled={props.disabled}
+          handleChange={props.handleChange}
+          value={props[label]} />
+      ))}
+    </div>
+  );
+};
